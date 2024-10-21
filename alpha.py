67736a64263fc6a55bad6a9957e4a7ea88499b34
Pallavi1162 <avanipallavi3@gmail.com>
2024-10-21 07:08:05 +0530

    Create alpha.py

---
+++ alpha.py
@@ -0,0 +1,23 @@
+'''
+Write a program to check whether the given character is vowel or consonant.
+Input format:
+The input consist of a character
+Output format:
+The output consists of a below-given string
+“Vowel” / “Consonant” / “Not an alphabet”
+Sample Input:
+e
+
+Sample Output:
+Vowel
+'''
+
+
+char = input().lower()
+if char.isalpha():
+    if char in 'aeiou':
+        print("Vowel")
+    else:
+        print("Consonant")
+else:
+    print("Not an alphabet")
